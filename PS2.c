6a20225f227d979962c79937d508ff5057944259
Denis <dudeofea@hotmail.com>
2014-02-08 22:02:46 +0000

    Started Interrupt Transferring

--- PS2.c
+++ PS2.c
@@ -6,6 +6,8 @@
 #define PS2_VENDOR_ID	0x0810
 #define PS2_PRODUCT_ID	0x0001
 
+#define PS2_ENDPOINT	0x81
+
 //find the first quckcam express camera attached to usb
 struct libusb_device *find_ps2_hub(libusb_context *context, libusb_device **devices){
 	
@@ -18,7 +20,8 @@ struct libusb_device *find_ps2_hub(libusb_context *context, libusb_device **devi
 		printf("Found %d USB devices\n", (int)cnt);
 	}
 	//Find PS2 Controller Hub
-	for (int i = 0; i < cnt; ++i)
+	for
+	 (int i = 0; i < cnt; ++i)
 	{
 		struct libusb_device_descriptor desc;
 		int r = libusb_get_device_descriptor(devices[i], &desc);
@@ -29,6 +32,28 @@ struct libusb_device *find_ps2_hub(libusb_context *context, libusb_device **devi
 		}
 	}
 	return NULL;
+}
+
+void get_ps2_input(libusb_device_handle* handle){
+	unsigned char data[8];
+	int trans = 0;
+	int err = libusb_interrupt_transfer(handle,
+		PS2_ENDPOINT,		//endpoint
+		data,				//data buffer
+		8,					//length
+		&trans,				//bytes transfered
+		500);				//timeout
+	if(err){
+		printf("Transfer Error, %s\n", libusb_error_name(err));
+	}else{
+		printf("%d Bytes Transfered\n", trans);
+		printf("Data: ");
+		for (int i = 0; i < 8; ++i)
+		{
+			printf("%02x", data[i]);
+		}
+		printf("\n");
+	}
 }
 
 int main(int argc, const char* argv[])
@@ -43,8 +68,8 @@ int main(int argc, const char* argv[])
 		int err = 0;
 		err = libusb_open(ps2, &handle);
 		if (err)
-			printf("Unable to open device, err %d\n", err);
-		//init the usb hub if neccesary
+			printf("Unable to open device, %s\n", libusb_error_name(err));
+		get_ps2_input(handle);
 	}
 	libusb_free_device_list(devices, 1);
 	libusb_exit(context);
