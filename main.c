64172ab79ad461174f748b4dc267e4998b416a5e
GermanRGomez <grgomez@ualberta.ca>
2014-02-09 07:59:47 -0800

    added dependencies

---
+++ main.c
@@ -0,0 +1,14 @@
+/* Main game loop */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <unistd.h>
+
+#include "bcm_host.h"
+
+#include "GLES/gl.h"
+#include "EGL/egl.h"
+#include "EGL/eglext.h"
