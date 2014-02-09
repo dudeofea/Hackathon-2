3451cdef37af270361e10fcf0fa565f890268985
GermanRGomez <grgomez@ualberta.ca>
2014-02-09 06:49:33 -0800

    changed textrender

--- video_test/fontrender/textrender.c
+++ video_test/fontrender/textrender.c
@@ -37,50 +37,20 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "vgfont.h"
 
 
-static const char *strnchr(const char *str, size_t len, char c)
+int32_t render_subtitle(GRAPHICS_RESOURCE_HANDLE img, const char *text, const uint32_t text_size, const uint32_t y_offset)
 {
-   const char *e = str + len;
-   do {
-      if (*str == c) {
-         return str;
-      }
-   } while (++str < e);
-   return NULL;
-}
-
-int32_t render_subtitle(GRAPHICS_RESOURCE_HANDLE img, const char *text, const int skip, const uint32_t text_size, const uint32_t y_offset)
-{
-   uint32_t text_length = strlen(text)-skip;
+  uint32_t text_length = strlen(text);
    uint32_t width=0, height=0;
-   const char *split = text;
    int32_t s=0;
    int len = 0; // length of pre-subtitle
    uint32_t img_w, img_h;
 
    graphics_get_resource_size(img, &img_w, &img_h);
 
-   if (text_length==0)
-      return 0;
-   while (split[0]) {
-     s = graphics_resource_text_dimensions_ext(img, split, text_length /*text_length-(split-text)*/, &width, &height, text_size);
-      if (s != 0) return s;
-      
-      /*
-      if (width > img_w) {
-         const char *space = strnchr(split, text_length-(split-text), ' ');
-         if (!space) {
-            len = split+1-text;
-            split = split+1;
-         } else {
-            len = space-text;
-            split = space+1;
-         }
-      } else {
-         break;
-      }
-      */
-      
-   }
+   s = graphics_resource_text_dimensions_ext(img, text, text_length, &width, &height, text_size);
+   if (text_length==0) return 0;
+   if (s != 0) return s;
+   
    // split now points to last line of text. split-text = length of initial text. text_length-(split-text) is length of last line
    if (width) {
       s = graphics_resource_render_text_ext(img, (img_w - width)>>1, y_offset-height,
@@ -91,7 +61,7 @@ int32_t render_subtitle(GRAPHICS_RESOURCE_HANDLE img, const char *text, const in
                                      split, text_length-(split-text), text_size);
       if (s!=0) return s;
    }
-   return render_subtitle(img, text, skip+text_length-len, text_size, y_offset - height);
+   return render_subtitle(img, text, text_length, text_size, y_offset);
 }
 
 int main(void)
